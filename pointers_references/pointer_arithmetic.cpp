2a05ca06007850cbabcb38d12ab23ca5fb68d63c
John Gahagan <109774527+SaviorFs@users.noreply.github.com>
2025-10-17 19:18:45 -0400

    additional examples

--- pointers_references/pointer_arithmetic.cpp
+++ pointers_references/pointer_arithmetic.cpp
@@ -3,13 +3,39 @@ using namespace std;
 
 int main() {
     int arr[5] = {10, 20, 30, 40, 50};
-    int *p = arr;  // this is the same as &arr[0]
+    
+    // Pointer points to the first element in the array
+    int* p = arr;
 
-    cout << *p << endl;  // 10
+    cout << "Pointer Arithmetic Demo\n\n";
+
+    // Show initial value
+    cout << "p points to value: " << *p << endl;
+
+    // Move forward in memory (by sizeof(int) each time)
     p++;
-    cout << *p << endl;  // 20
+    cout << "After p++: " << *p << endl;
+
     p++;
-    cout << *p << endl;  // 30
+    cout << "After another p++: " << *p << endl;
+
+    // Move backward
+    p--;
+    cout << "After p--: " << *p << endl;
+
+    // Pointer difference
+    int* start = arr;
+    int* end = &arr[4];
+    cout << "\nDifference between end and start: " << (end - start)
+         << " elements apart.\n";
+
+    // Iterate through entire array using a pointer
+    cout << "\nIterating through array with pointer:\n";
+    int* iter = arr;
+    for (int i = 0; i < 5; i++) {
+        cout << "Element " << i << ": " << *iter << endl;
+        iter++;
+    }
 
     return 0;
 }
