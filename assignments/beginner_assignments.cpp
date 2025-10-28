af504f25fa6b966de545d28fb3984c52a6f39697
John Gahagan <109774527+SaviorFs@users.noreply.github.com>
2025-10-28 19:47:20 -0400

    small assignment for tonight

--- assignments/beginner_assignments.cpp
+++ assignments/beginner_assignments.cpp
@@ -1,26 +1,53 @@
 #include <iostream>
 using namespace std;
-//assignment 1
-int SumOfTwo (int a, int b) {
+
+// assignment 1
+int SumOfTwo(int a, int b) {
     return a + b;
 }
-//assignment 2
-void EvenOrOdd (int c) {
-    if (c % 2 == 0) {
+
+// assignment 2
+void EvenOrOdd(int c) {
+    if (c % 2 == 0)
         cout << "Even" << endl;
-    }
-    else {
+    else
         cout << "Odd" << endl;
+}
+
+// Calculator functions
+int add(int a, int b) { return a + b; }
+int subtract(int a, int b) { return a - b; }
+int multiply(int a, int b) { return a * b; }
+
+double divide(double a, double b) {
+    if (b == 0) {
+        cout << "Can't divide by 0" << endl;
+        return 0;
+    } else {
+        return a / b;
     }
 }
 
-main() {
+int main() {
     int num1, num2;
+    char op;
+
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
-    cout << "Sum = " << SumOfTwo(num1,num2) << endl;
-    cout << "Checking first number: ";
-    EvenOrOdd(num1);
+
+    cout << "Choose an operation (+, -, *, /): ";
+    cin >> op;
+
+    if (op == '+')
+        cout << "Result: " << add(num1, num2) << endl;
+    else if (op == '-')
+        cout << "Result: " << subtract(num1, num2) << endl;
+    else if (op == '*')
+        cout << "Result: " << multiply(num1, num2) << endl;
+    else if (op == '/')
+        cout << "Result: " << divide(num1, num2) << endl;
+    else
+        cout << "Invalid operator" << endl;
 
     return 0;
 }
